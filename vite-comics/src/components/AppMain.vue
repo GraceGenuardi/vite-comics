8a45259c9a6e0518b3cb9996ceff3b0e42b3a244
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-01 15:00:54 +0100

    EDIT + STYLE finita struttura main

--- vite-comics/src/components/AppMain.vue
+++ vite-comics/src/components/AppMain.vue
@@ -1,9 +1,61 @@
 <template>
     <main class="content">
       <div class="container">
-        <p>
-          contenuto prodotti
-        </p>
+        <!-- Diviosione in 4 righe -->
+
+        <div class="row-content">
+          <h3>Content goes here</h3>
+        </div>
+
+        <div class="row-brands">
+
+          <ul class="list-brand">
+            <li>Brand+icona</li>
+            <li>Brand+icona</li>
+            <li>Brand+icona</li>
+            <li>Brand+icona</li>
+            <li>Brand+icona</li>
+          </ul>
+
+
+        </div>
+
+
+        <div class="row-hero"> 
+
+          <ul class="list-hero">
+            <li class="list-title"> <h4>TV</h4></li>
+            <li>tv</li>
+            <li>tv</li>
+            <li>tv</li>
+            <li>tv</li>
+            <li>tv</li>
+            <li>tv</li>
+            <li><h4>Marvel</h4></li>
+            <li>tv</li>
+            <li>tv</li>
+          </ul>
+
+          <ul class="list-hero">
+            <li class="list-title"><h4>Games</h4></li>
+            <li>DC</li>
+            <li>DC</li>
+            <li>DC</li>
+            <li>DC</li>
+            <li>DC</li>
+            <li>DC</li>
+          </ul>
+
+          <ul class="list-hero">
+            <li class="list-title"> <h4>Video</h4></li>
+            <li>VIDEO</li>
+            <li>GAMES</li>
+            <li>GAMES</li>
+            <li>GAMES</li>
+          </ul>
+
+        </div>
+        
       </div>
     </main>
   </template>
