ce34b58e7d8fd8ed691ebc655f34daf16b9b25d6
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-01 14:16:23 +0100

    EDIT divisione in tre blocchi

---
+++ vite-comics/src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="main-header">
+      <div class="container">
+        <img src="/images/la-molisana-logo.png" alt="">
+  
+        <nav class="main-menu">
+          <ul>
+            <li class="menu-item">Home</li>
+            <li class="menu-item active">Prodotti</li>
+            <li class="menu-item">Chi Siamo</li>
+            <li class="menu-item">Contatti</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </template>
+  
+  <script>
+    export default {
+      
+    }
+  </script>
+  
+  
---
+++ vite-comics/src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="content">
+      <div class="container">
+        <p>
+          contenuto prodotti
+        </p>
+      </div>
+    </main>
+  </template>
+  
+  <script>
+    export default {
+      
+    }
+  </script>
+  
+  
