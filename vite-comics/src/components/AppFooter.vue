86488988982300b4e6024a8a1ac95c7177eba747
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-02 19:11:40 +0100

    EDIT footer completato

--- vite-comics/src/components/AppFooter.vue
+++ vite-comics/src/components/AppFooter.vue
@@ -7,7 +7,14 @@
         </div>
         <div class="col-footer">
           <h4>follow us</h4>
-          <p>lista social</p>
+          <ul class="social-list">
+            <li><img src="../assets/img/footer-facebook.png" alt="FB"></li>
+            <li><img src="../assets/img/footer-periscope.png" alt="PERISCOPE"></li>
+            <li><img src="../assets/img/footer-pinterest.png" alt="PINTEREST"></li>
+            <li><img src="../assets/img/footer-twitter.png" alt="TWITTER"> </li>
+            <li><img src="../assets/img/footer-youtube.png" alt="YOUTUBE"></li>
+
+          </ul>
          
         </div>
       </div>
